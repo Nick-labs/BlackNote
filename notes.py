9e42dedaeecce6e4b6c5704f562ae5d5013651dd
Nick-labs <nikita.fomin.0404@ya.ru>
2020-11-13 21:00:46 +0300

    Added tracking drag of note

--- notes.py
+++ notes.py
@@ -10,22 +10,26 @@ conn = sqlite3.connect('notes.db')
 cursor = conn.cursor()
 
 
-class Note(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def create_new_note():
+    main()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('note.ui', self)
         self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-        self.closeButton.setStyleSheet('''background-color: gray;
-                                          border-style: outset;
-                                          border-width: 1px;
-                                          border-color: gray;
-                                          font: bold 10px;
-                                          padding: 6px''')
+        # self.closeButton.setStyleSheet('''background-color: gray;
+        #                                   border-style: outset;
+        #                                   border-width: 1px;
+        #                                   border-color: gray;
+        #                                   font: bold 10px;
+        #                                   padding: 6px''')
         self.textEdit.setStyleSheet('border: 0')
         self.load_notes()
 
-
         self.closeButton.pressed.connect(self.delete_window)
+        # self.newButton.pressed.connect(create_new_note)
         self.textEdit.textChanged.connect(self.save)
 
     def save(self):
@@ -54,12 +58,20 @@ class Note(QMainWindow):
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = event.globalPos()
 
+        self.drag_active = True
+
+    def mouseReleaseEvent(self, event):
+        if self.drag_active:
+            # self.save()
+            print('drag')
+            self.drag_active = False
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
     palette = QPalette()
@@ -68,10 +80,14 @@ if __name__ == '__main__':
     # palette.setColor(QPalette.ButtonText, QColor(121, 85, 72))
     # palette.setColor(QPalette.Text, QColor('#AAAAAA'))
     palette.setColor(QPalette.Base, QColor('#AAAAAA'))
-    #palette.setColor(QPalette.AlternateBase, QColor(188, 170, 164))
+    # palette.setColor(QPalette.AlternateBase, QColor(188, 170, 164))
     app.setPalette(palette)
-    ex = Note()
+    ex = MainWindow()
     ex.show()
     sys.excepthook = except_hook
-    conn.close()
     sys.exit(app.exec())
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
