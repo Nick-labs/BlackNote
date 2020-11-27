ea34cb2534b9146f3128e9b8cc1580c3b07ef107
Nick-labs <nikita.fomin.0404@ya.ru>
2020-11-27 16:45:17 +0300

    Change color of note added

--- notes.py
+++ notes.py
@@ -13,11 +13,14 @@ cursor = conn.cursor()
 class MainWindow(QMainWindow):
     def __init__(self, note=None, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+
         uic.loadUi('note.ui', self)
+
         self.setFixedSize(220, 260)
         self.settingsButton.setText(u'\u2699')
         self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
         self.textEdit.setStyleSheet('border: 0')
+        self.setStyleSheet(f'background-color: {note[4]};')
 
         self.note = note
         self.textEdit.setText(self.note[1])
@@ -25,14 +28,15 @@ class MainWindow(QMainWindow):
 
         self.menubar_is_active = False
 
+        'Создаем системный трей'
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_FileIcon))
 
         '''
-            Объявим и добавим действия для работы с иконкой системного трея
+            Объявляем и добавляем действия для работы с иконкой системного трея
             show - показать окно
             hide - скрыть окно
-            exit - выход из программы
+            close program - выход из программы
         '''
 
         show_action = QAction("Show", self)
@@ -48,55 +52,105 @@ class MainWindow(QMainWindow):
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
 
-        exitAction = QAction('&Close', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.triggered.connect(self.close_window)
+        """Создаем события для menubar"""
+        exit_action = QAction('&Close note', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.triggered.connect(self.close_window)
 
-        saveAction = QAction('&Save', self)
-        saveAction.setShortcut('Ctrl+S')
-        saveAction.triggered.connect(self.save)
+        save_action = QAction('&Save', self)
+        save_action.setShortcut('Ctrl+S')
+        save_action.triggered.connect(self.save)
 
+        close_program_action = QAction('&Close program', self)
+        close_program_action.triggered.connect(self.close_program)
+
+        change_color_white = QAction('White', self)
+        change_color_white.triggered.connect(self.change_color)
+
+        change_color_gray = QAction('Gray', self)
+        change_color_gray.triggered.connect(self.change_color)
+
+        change_color_red = QAction('Red', self)
+        change_color_red.triggered.connect(self.change_color)
+
+        change_color_green = QAction('Green', self)
+        change_color_green.triggered.connect(self.change_color)
+
+        change_color_blue = QAction('Blue', self)
+        change_color_blue.triggered.connect(self.change_color)
+
+        'Создаем menubar и добавляем в него раннее созданные действия'
         menubar = self.menuBar()
         menubar.hide()
         fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAction)
-        fileMenu.addAction(saveAction)
+        fileMenu.addAction(save_action)
+        fileMenu.addAction(exit_action)
+        fileMenu.addAction(close_program_action)
+        changeColorMenu = fileMenu.addMenu('&Change color')
+        changeColorMenu.addAction(change_color_white)
+        changeColorMenu.addAction(change_color_gray)
+        changeColorMenu.addAction(change_color_red)
+        changeColorMenu.addAction(change_color_green)
+        changeColorMenu.addAction(change_color_blue)
 
         self.drag_active = False
 
+        'Коннектим кнопки к определенным событиям'
         self.closeButton.pressed.connect(self.close_window)
         self.settingsButton.pressed.connect(self.menubar_change)
         self.textEdit.textChanged.connect(self.save)
 
+    def change_color(self):
+        """Смена цвета заметки"""
+
+        color = self.sender().text()
+
+        if color == 'White':
+            color = '#FFFFFF'
+        elif color == 'Gray':
+            color = '#AAAAAA'
+        elif color == 'Red':
+            color = '#FF6868'
+        elif color == 'Green':
+            color = '#A5FF91'
+        else:
+            color = '#7878CA'
+
+        self.setStyleSheet(f'background-color: {color};')
+        cursor.execute(f'''UPDATE notes SET color = "{color}" WHERE id = {self.note[0]}''')
+
     def save(self):
-        print(f'saved "{self.textEdit.toPlainText()}"')
+        """Сохранение заметки в базу данных"""
+
         cursor.execute(
-            f"""UPDATE notes SET text = "{self.textEdit.toPlainText()}",
-                x = {self.x()}, y = {self.y()} WHERE id = {self.note[0]}""")
+            f'''UPDATE notes SET text = "{self.textEdit.toPlainText()}",
+                x = {self.x()}, y = {self.y()} WHERE id = {self.note[0]}''')
         conn.commit()
 
     def close_program(self):
+        """Завершение программы"""
+
         QCoreApplication.exit(0)
 
-    def close_window_without_question(self):
-        self.close()
-        self.save()
-
     def close_window(self):
-        print('close')
+        """Закрытие(сворачивание) окна"""
+
         result = QMessageBox.question(self, 'Confirm close', 'Are you sure you want to close this note?')
         if result == QMessageBox.Yes:
             self.close()
             self.save()
-            print('closed')
 
     def menubar_change(self):
+        """Прячем или показываем menubar"""
+
         if not self.menubar_is_active:
             self.menuBar().show()
             self.menubar_is_active = True
         else:
             self.menuBar().hide()
             self.menubar_is_active = False
+
+    """Далее методы для перемещения окна мышкой"""
 
     def mousePressEvent(self, event):
         self.oldPos = event.globalPos()
@@ -110,11 +164,12 @@ class MainWindow(QMainWindow):
     def mouseReleaseEvent(self, event):
         if self.drag_active:
             self.save()
-            print('drag')
             self.drag_active = False
 
 
 def load_notes():
+    """Загружаем записки из базы данных"""
+
     cursor.execute("SELECT * FROM notes")
     notes = cursor.fetchall()
     print(notes)
@@ -129,7 +184,7 @@ def main():
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
 
-    app.setStyle("Fusion")
+    app.setStyle('Fusion')
     palette = QPalette()
     palette.setColor(QPalette.Window, QColor('#AAAAAA'))
     palette.setColor(QPalette.Base, QColor('#AAAAAA'))
